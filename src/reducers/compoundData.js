f477a0214da0a4658e8564eaf272c8d86f19b714
dr. Lantos Tibor <lantost@intranet.local>
2019-02-07 13:52:24 +0100

    FEAT: add request action to reducer as well

--- src/reducers/compoundData.js
+++ src/reducers/compoundData.js
@@ -1,13 +1,22 @@
 export default function compoundData(state = {
+  loading: false,
   name: null,
   img: null,
 }, action) {
   switch (action.type) {
+    case 'COMPOUND_RESULT_REQUESTED': {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+
     case 'COMPOUND_RESULT_SUCCEEDED': {
       return {
         ...state,
-        name: action.payload.compoundName,
+        name: action.payload.name,
         img: action.payload.data.image,
+        loading: false,
       };
     }
 
